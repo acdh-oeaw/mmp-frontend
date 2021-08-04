dd0c6fbc152941be64da044fe86f2f1e53996d6a
Robin <robinkaggl@hotmail.de>
2021-08-04 15:10:28 +0200

    added i18n and vuetify, changed folder structure

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import App from './App';
+import router from './plugins/router';
+import store from './plugins/store';
+import vuetify from './plugins/vuetify';
+import i18n from './plugins/i18n';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify',
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'de',
+      localeDir: 'locales',
+      enableInSFC: true,
+    },
+  },
+};
