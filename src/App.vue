8377823ea47e66ff66989a648050b1382adebc96
Robin <robinkaggl@hotmail.de>
2021-08-09 15:01:59 +0200

    Added Sidebar for mobile view, updated file tree

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,6 @@
 <template>
   <v-app class="app-class">
-    <v-app-bar
-      flat
-      elevate-on-scroll
-      shrink-on-scroll
-      fixed
-      dark
-      prominent
-      color="#0f1226"
-      class="app-bar"
-      min-height="84px"
-    >
-      <v-row justify="center">
-        <v-col cols="12" md="2" class="title-wrapper">
-          <v-app-bar-nav-icon class="d-inline d-md-none" />
-          <v-toolbar-title class="d-inline fancy-font thick">
-            MMP
-          </v-toolbar-title>
-        </v-col>
-        <v-col cols="8" class="text-right d-none d-md-inline">
-          <div>
-            <router-link color="white" :to="{ name: '', params: {} }">
-              About the Project
-            </router-link>
-            <span class="non-selectable">
-              &nbsp;&nbsp;&bull;&nbsp;&nbsp;
-            </span>
-            <router-link :to="{ name: '', params: {} }">
-              Case Studies
-            </router-link>
-            <span class="non-selectable">
-              &nbsp;&nbsp;&bull;&nbsp;&nbsp;
-            </span>
-            <router-link :to="{ name: '', params: {} }">
-              Explore the Data
-            </router-link>
-          </div>
-        </v-col>
-      </v-row>
-    </v-app-bar>
+    <app-bar />
     <v-main>
       <router-view />
     </v-main>
@@ -46,40 +8,21 @@
 </template>
 
 <script>
+import AppBar from './components/AppBar';
 
 export default {
   name: 'App',
 
   data: () => ({
-    //
+
   }),
+  components: {
+    AppBar,
+  },
 };
 </script>
 <style lang="css">
-  a {
-    color: white !important;
-    text-decoration: none;
-  }
   .app-class {
     background-color: #f1f5fa !important;
-  }
-  .app-bar {
-    padding-top: 30px;
-  }
-  .fancy-font {
-    font-family: "Times New Roman", Times, serif;
-    font-size: 2em !important;
-  }
-  .non-selectable {
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
-  .thick {
-    font-weight: bold;
-  }
-  .title-wrapper {
-    padding: 0px !important;
   }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-navigation-drawer
+      v-model="$store.state.interface.sidebarDrawer"
+      color="#f1f5fa"
+      fixed
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon @click="$store.commit('toggleDrawer')">mdi-close</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6 sidebar-title">
+            MMP
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>About the Project</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Case Studies</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Explore the Data</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+};
+</script>
+
+<style scoped>
+  div.sidebar-title {
+    font-family: "Times New Roman", Times, serif !important;
+    font-weight: bold;
+  }
+</style>
