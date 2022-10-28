41191dd329fb6f91cfdeccea7f168a0636724601
stefanprobst <stefanprobst@posteo.de>
2022-10-28 08:48:25 +0200

    build: add meta tags (#112)
    * build: add meta tags
    
    * build: use self-hosted fonts, use variable roboto font

--- vue.config.mjs
+++ vue.config.mjs
@@ -1,14 +1,38 @@
 import { defineConfig } from '@vue/cli-service';
 
+import { metadata } from './config/metadata.config.mjs';
+
+const publicPath = '/mmp-frontend/';
+const canonicalUrl = new URL(
+  publicPath,
+  process.env['VUE_APP_BASE_URL'] ?? 'https://acdh-oeaw.github.io'
+).toString();
+
 const config = defineConfig({
   chainWebpack(config) {
     /**
      * @see https://github.com/vuejs/vue-cli/issues/3123
      */
     config.resolve.alias.set('~', process.cwd());
+
+    config.plugin('html').tap((args) => {
+      args[0].title = metadata.title;
+      args[0].meta = {
+        description: metadata.description,
+        'og:type': 'website',
+        'og:locale': 'en',
+        'og:title': metadata.title,
+        'og:description': metadata.description,
+        'og:url': canonicalUrl,
+      };
+      args[0].templateParameters = {
+        canonicalUrl,
+      };
+      return args;
+    });
   },
   productionSourceMap: false,
-  publicPath: '/mmp-frontend/',
+  publicPath,
   transpileDependencies: true,
 });
 
