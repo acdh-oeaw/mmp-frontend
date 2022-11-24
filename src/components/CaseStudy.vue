323e5a6104984eff2fdb7e7960020fc379c7cada
Robin Kaggl <robin.kaggl@oeaw.ac.at>
2022-11-24 17:25:14 +0100

    finished texts & authors tab in case studies

--- src/components/CaseStudy.vue
+++ src/components/CaseStudy.vue
@@ -83,37 +83,66 @@
               <word-cloud-wrapper :usecase="id || $route.params.id" />
             </v-tab-item>
             <v-tab-item value="texts">
-              <v-expansion-panels>
-                <v-expansion-panel v-for="author in authors" :key="author.id">
-                  <v-card>
-                    <v-card-title>
-                      {{ getOptimalName(author) }}
-                    </v-card-title>
-                    <v-card-text>
-                      <ul>
-                        <li
-                          v-for="text in texts.filter((text) =>
-                            text.autor.map((autor) => autor.id).includes(author.id)
-                          )"
-                          :key="text.id"
-                        >
-                          {{ text.title }}
-                          <ul>
-                            <li
-                              v-for="passage in passages.results.filter(
-                                (passage) => passage.text.id === text.id
-                              )"
-                              :key="passage.id"
-                            >
-                              {{ passage.display_label }}
-                            </li>
-                          </ul>
-                        </li>
-                      </ul>
-                    </v-card-text>
-                  </v-card>
-                </v-expansion-panel>
-              </v-expansion-panels>
+              <v-list color="transparent">
+                <div v-for="author in authors" :key="author.id">
+                  <v-list-group prepend-icon="mdi-account-edit">
+                    <template #activator>
+                      <v-list-item>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            {{ getOptimalName(author) }}
+                          </v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ getTextsByAuthor(author.id).length }}
+                            text{{ getTextsByAuthor(author.id).length === 1 ? '' : 's' }} and
+                          </v-list-item-subtitle>
+                          <v-list-item-subtitle>
+                            {{ getPassagesByAuthor(author.id).length }} passage{{
+                              getPassagesByAuthor(author.id).length === 1 ? '' : 's'
+                            }}
+                            found in this case study
+                          </v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </template>
+                    <v-list-group
+                      v-for="text in getTextsByAuthor(author.id)"
+                      :key="text.id"
+                      :ripple="false"
+                      prepend-icon="mdi-book-open-variant"
+                      sub-group
+                      no-action
+                    >
+                      <template #activator>
+                        <v-list-item>
+                          <v-list-item-content>
+                            <v-list-item-title>
+                              {{ text.title }}
+                            </v-list-item-title>
+                            <v-list-item-subtitle>
+                              {{ getPassagesByText(text.id).length }} passage{{
+                                getPassagesByText(text.id).length === 1 ? '' : 's'
+                              }}
+                            </v-list-item-subtitle>
+                          </v-list-item-content>
+                        </v-list-item>
+                      </template>
+                      <v-list-item
+                        v-for="passage in getPassagesByText(text.id)"
+                        :key="passage.id"
+                        :to="{
+                          name: 'List',
+                          query: addParamsToQuery({ Passage: passage.id }),
+                        }"
+                        prepend-icon="mdi-format-quote-close"
+                      >
+                        {{ passage.display_label }}
+                      </v-list-item>
+                    </v-list-group>
+                  </v-list-group>
+                  <v-divider inset />
+                </div>
+              </v-list>
             </v-tab-item>
           </v-tabs-items>
         </template>
@@ -161,7 +190,7 @@ export default {
     const urls = [
       `${import.meta.env.VITE_APP_MMP_API_BASE_URL}/api/usecase/${id}?format=json`,
       `${import.meta.env.VITE_APP_MMP_API_BASE_URL}/archiv/usecase-timetable-data/${id}`,
-      `${import.meta.env.VITE_APP_MMP_API_BASE_URL}/api/stelle/?use_case=${id}&limit=200`, // limit is not ideal
+      `${import.meta.env.VITE_APP_MMP_API_BASE_URL}/api/stelle/?use_case=${id}&limit=200`, // limit is not ideal, i already created a ticket for the backend to improve this
     ];
 
     const prefetched = this.$store.state.fetchedResults[urls.toString()];
@@ -192,6 +221,17 @@ export default {
     }
   },
   methods: {
+    getTextsByAuthor(authorID) {
+      return this.texts.filter((text) => text.autor.map((autor) => autor.id).includes(authorID));
+    },
+    getPassagesByText(textID) {
+      return this.passages.results.filter((passage) => passage.text.id === textID);
+    },
+    getPassagesByAuthor(authorID) {
+      return this.passages.results.filter((passage) =>
+        passage.text.autor.map((autor) => autor.id).includes(authorID)
+      );
+    },
     removeDatesFromTitle(title) {
       const ret = title.split(' ');
       ret.shift();
