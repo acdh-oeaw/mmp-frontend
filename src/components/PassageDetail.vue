1d6704dae4ab8db9981ba1bf3ee38e6eb62a1f82
Stefan Probst <stefanprobst@posteo.de>
2023-01-29 22:47:16 +0100

    refactor: use composition api in passage detail

--- src/components/PassageDetail.vue
+++ src/components/PassageDetail.vue
@@ -1,9 +1,100 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router/composables';
+
+import { type Author, type Place, usePassageById } from '@/api';
+import { getAuthorLabel, getPlaceLabel } from '@/lib/get-label';
+import { keywordColors } from '@/lib/search/search.config';
+import { useDrawerWidth } from '@/lib/use-drawer-width';
+import { useFullScreen } from '@/lib/use-full-screen';
+import { useStore } from '@/lib/use-store';
+
+const route = useRoute();
+const id = computed(() => Number(route.params.id));
+
+const store = useStore();
+
+const passageQuery = usePassageById({ id });
+
+const isLoading = computed(() => passageQuery.isInitialLoading.value);
+const passage = computed(() => passageQuery.data.value);
+
+const title = computed(() => {
+  if (passage.value == null) return {};
+
+  return {
+    title: passage.value.text?.title,
+    written:
+      passage.value.text?.start_date || passage.value.text?.end_date
+        ? `${passage.value.text.start_date || 'unknown'} - ${
+            passage.value.text.end_date || 'unknown'
+          }`
+        : 'unknown',
+    author: passage.value.text?.autor.map((x) => x.name_en).join(', '),
+  };
+});
+
+const items = computed(() => {
+  if (passage.value == null) return [];
+
+  return [
+    {
+      key: 'Keywords',
+      value: passage.value.key_word,
+    },
+    {
+      key: 'Translation',
+      value: passage.value.translation,
+    },
+    {
+      key: 'Original quote',
+      value: passage.value.zitat,
+    },
+    {
+      key: 'Title',
+      value: title.value.title,
+    },
+    {
+      key: 'Cited',
+      value: passage.value.zitat_stelle,
+    },
+    {
+      key: 'Summary',
+      value: passage.value.summary,
+    },
+    {
+      key: 'Author',
+      value: passage.value.text?.autor,
+    },
+    {
+      key: 'Place',
+      value: passage.value.text?.ort,
+    },
+    {
+      key: 'Edition',
+      value: passage.value.text?.edition,
+    },
+    {
+      key: 'Text written in',
+      value: title.value.written,
+    },
+    {
+      key: 'Comment',
+      value: passage.value.kommentar,
+    },
+  ] as const;
+});
+
+const isFullScreen = useFullScreen();
+const drawerWidth = useDrawerWidth();
+</script>
+
 <template>
   <v-navigation-drawer permanent fixed right color="#F1F5FA" :width="drawerWidth">
     <v-list-item>
       <v-list-item-action>
         <router-link
-          :to="{ name: isFullScreen ? 'List Fullscreen' : 'List', query: $route.query }"
+          :to="{ name: isFullScreen ? 'List Fullscreen' : 'List', query: route.query }"
           class="text-decoration-none"
         >
           <v-icon>mdi-close</v-icon>
@@ -25,17 +116,17 @@
     <v-divider />
     <v-simple-table v-if="!isLoading" class="data-table">
       <tbody>
-        <tr v-for="item in items" :key="item.key">
+        <tr v-for="item of items" :key="item.key">
           <td class="grey--text text--darken-1">
             {{ item.key }}
           </td>
           <td v-if="item.key === 'Keywords'">
-            <div v-for="keyword in item.value" :key="keyword.id" class="keyword-chip">
+            <div v-for="keyword of item.value" :key="keyword.id" class="keyword-chip">
               <v-chip
-                :color="keyColors.chips[keyword.art]"
+                :color="keywordColors[keyword.art]"
                 small
                 @click="
-                  $store.commit('addToItemsAndInput', {
+                  store.commit('addToItemsAndInput', {
                     id: keyword.id,
                     selected_text: keyword.stichwort,
                     group: 'Keyword',
@@ -46,18 +137,20 @@
               </v-chip>
             </div>
           </td>
-          <td v-else-if="['Place', 'Author'].includes(item.key)">
+          <td v-else-if="item.key === 'Place' || item.key === 'Author'">
             <router-link
-              v-for="(val, i) in item.value"
+              v-for="(val, i) of item.value"
               :key="val.id"
               :to="{
                 name: isFullScreen ? `${item.key} Detail Fullscreen` : `${item.key} Detail`,
-                query: item.key === 'Place' ? addParamsToQuery({ Place: val.id }) : $route.query,
+                query: item.key === 'Place' ? { ...route.query, Place: val.id } : route.query,
                 params: { id: val.id },
               }"
             >
               <span v-if="i != 0">, </span>
-              {{ getOptimalName(val) }}
+              {{
+                item.key === 'Place' ? getPlaceLabel(val as Place) : getAuthorLabel(val as Author)
+              }}
               <v-icon>mdi-chevron-right</v-icon>
             </router-link>
           </td>
@@ -72,104 +165,3 @@
     </v-container>
   </v-navigation-drawer>
 </template>
-
-<script>
-import { computed } from 'vue';
-import { useRoute } from 'vue-router/composables';
-
-import { usePassageById } from '@/api';
-import helpers from '@/helpers';
-
-export default {
-  name: 'PassasgeDetail',
-  mixins: [helpers],
-  setup() {
-    const route = useRoute();
-    const id = computed(() => Number(route.params.id));
-
-    const passageQuery = usePassageById({ id });
-
-    const isLoading = computed(() => passageQuery.isInitialLoading.value);
-    const passage = computed(() => passageQuery.data.value);
-
-    const title = computed(() => {
-      if (passage.value == null) return {};
-
-      return {
-        title: passage.value.text.title,
-        written:
-          passage.value.text.start_date || passage.value.text.end_date
-            ? `${passage.value.text.start_date || 'unknown'} - ${
-                passage.value.text.end_date || 'unknown'
-              }`
-            : 'unknown',
-        author: passage.value.text.autor.map((x) => x.name_en).join(', '),
-      };
-    });
-
-    const items = computed(() => {
-      if (passage.value == null) return [];
-
-      return [
-        {
-          key: 'Keywords',
-          value: passage.value.key_word,
-        },
-        {
-          key: 'Translation',
-          value: passage.value.translation,
-        },
-        {
-          key: 'Original quote',
-          value: passage.value.zitat,
-        },
-        {
-          key: 'Title',
-          value: title.value.title,
-        },
-        {
-          key: 'Cited',
-          value: passage.value.zitat_stelle,
-        },
-        {
-          key: 'Summary',
-          value: passage.value.summary,
-        },
-        {
-          key: 'Author',
-          value: passage.value.text.autor,
-        },
-        {
-          key: 'Place',
-          value: passage.value.text.ort,
-        },
-        {
-          key: 'Edition',
-          value: passage.value.text.edition,
-        },
-        {
-          key: 'Text written in',
-          value: title.value.written,
-        },
-        {
-          key: 'Comment',
-          value: passage.value.kommentar,
-        },
-      ];
-    });
-
-    return {
-      isLoading,
-      passage,
-      items,
-      title,
-    };
-  },
-  data: () => ({
-    headers: [
-      { text: 'key', value: 'key' },
-      { text: 'value', value: 'value' },
-    ],
-  }),
-};
-</script>
