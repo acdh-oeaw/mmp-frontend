560af46b1402f48ca0d6ae970828a4ea9f75d4c5
Stefan Probst <stefanprobst@posteo.de>
2023-01-29 21:21:14 +0100

    refactor: use composition api in keyword list item and over time

--- src/components/KeywordListItem.vue
+++ src/components/KeywordListItem.vue
@@ -1,3 +1,41 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router/composables';
+
+import { type Keyword, usePassages } from '@/api';
+import { getAuthorLabel } from '@/lib/get-label';
+import { useFullScreen } from '@/lib/use-full-screen';
+import { useStore } from '@/lib/use-store';
+
+const props = defineProps<{
+  parentNodes: Array<Keyword['id']>;
+  siblingNode: Keyword['id'];
+}>();
+
+const route = useRoute();
+const store = useStore();
+
+const passagesQuery = usePassages(
+  computed(() => ({
+    [store.state.apiParams.intersect ? 'key_word_and' : 'key_word']: [
+      props.siblingNode,
+      ...props.parentNodes,
+    ],
+    has_usecase: store.state.apiParams.hasUsecase === 'true',
+  }))
+);
+
+const isLoading = computed(() => {
+  return passagesQuery.isInitialLoading.value;
+});
+
+const passages = computed(() => {
+  return passagesQuery.data.value?.results ?? [];
+});
+
+const isFullScreen = useFullScreen();
+</script>
+
 <template>
   <v-card flat color="rgba(0, 0, 0, 0)">
     <v-list two-line>
@@ -6,72 +44,38 @@
         type="list-item-three-line@3"
         class="transparent-skeleton"
       />
-      <v-list-item
-        v-for="passage in data"
-        v-else-if="data.length"
-        :key="passage.id"
-        three-line
-        :to="{
-          name: isfullscreen ? 'Passage Detail Fullscreen' : 'Passage Detail',
-          query: addParamsToQuery({ Passage: passage.id }),
-          params: { id: passage.id },
-        }"
-      >
-        <v-list-item-content>
-          <v-list-item-title>
-            {{ passage.display_label }}
-          </v-list-item-title>
-          <v-list-item-subtitle v-if="passage.text?.autor?.length">
-            {{ passage.text.title }},
-            {{ passage.text.autor.map((x) => getOptimalName(x)).join(', ') }}
-          </v-list-item-subtitle>
-          <v-list-item-subtitle v-if="passage.text?.jahrhundert">
-            {{ passage.text.jahrhundert }} century
-          </v-list-item-subtitle>
-        </v-list-item-content>
-        <v-list-item-icon>
-          <v-icon>mdi-chevron-right</v-icon>
-        </v-list-item-icon>
-      </v-list-item>
+      <template v-else-if="passages.length">
+        <v-list-item
+          v-for="passage of passages"
+          :key="passage.id"
+          three-line
+          :to="{
+            name: isFullScreen ? 'Passage Detail Fullscreen' : 'Passage Detail',
+            query: { ...route.query, Passage: passage.id },
+            params: { id: passage.id },
+          }"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ passage.display_label }}
+            </v-list-item-title>
+            <v-list-item-subtitle v-if="passage.text?.autor?.length">
+              {{ passage.text.title }},
+              {{ passage.text.autor.map(getAuthorLabel).join(', ') }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle v-if="passage.text?.jahrhundert">
+              {{ passage.text.jahrhundert }} century
+            </v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-icon>
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+      </template>
       <v-list-item v-else>No passages found!</v-list-item>
     </v-list>
   </v-card>
 </template>
-
-<script>
-import { computed } from 'vue';
-
-import { usePassages } from '@/api';
-import helpers from '@/helpers';
-import { useStore } from '@/lib/use-store';
-
-export default {
-  mixins: [helpers],
-  props: ['parentNodes', 'siblingNode'],
-  setup(props) {
-    const store = useStore();
-
-    const passagesQuery = usePassages(
-      computed(() => ({
-        [store.state.apiParams.intersect ? 'key_word_and' : 'key_word']: [
-          props.siblingNode,
-          ...props.parentNodes,
-        ],
-        has_usecase: store.state.apiParams.hasUsecase,
-      }))
-    );
-
-    const isLoading = computed(() => passagesQuery.isInitialLoading.value);
-
-    const passages = computed(() => passagesQuery.data.value?.results ?? []);
-
-    return {
-      isLoading,
-      data: passages,
-    };
-  },
-};
-</script>
 
 <style>
 div.transparent-skeleton > div {
--- src/components/KeywordOverTime.vue
+++ src/components/KeywordOverTime.vue
@@ -1,58 +1,56 @@
+<script lang="ts" setup>
+import { Chart } from 'highcharts-vue';
+import { computed } from 'vue';
+
+const props = defineProps<{
+  data: any;
+}>();
+
+const chartData = computed(() => {
+  return {
+    chart: {
+      backgroundColor: 'transparent',
+    },
+    credits: {
+      enabled: false,
+    },
+    title: {
+      text: '',
+    },
+    tooltip: {
+      // split: true,
+      // the only way the right 'this' object is passed to the formatter function
+
+      formatter: function lel(this: any) {
+        const endings = ['st', 'nd', 'rd'];
+        return `${this.point.x}${endings[this.point.x - 1] || 'th'} century<br />${
+          this.point.y
+        } occurences`;
+      },
+    },
+    legend: {
+      verticalAlign: 'top',
+    },
+    yAxis: {
+      title: {
+        text: '',
+      },
+    },
+    xAxis: {
+      labels: {
+        formatter: ({ value }: any) => value * 100,
+      },
+    },
+    series: props.data.map((x: any) => ({
+      name: x.title,
+      data: x.data,
+    })),
+  };
+});
+</script>
+
 <template>
   <div>
     <chart :options="chartData" />
   </div>
 </template>
-
-<script>
-import { Chart } from 'highcharts-vue';
-
-export default {
-  name: 'OverTime',
-  components: { Chart },
-  props: ['data'],
-  computed: {
-    chartData() {
-      return {
-        chart: {
-          backgroundColor: 'transparent',
-        },
-        credits: {
-          enabled: false,
-        },
-        title: {
-          text: '',
-        },
-        tooltip: {
-          // split: true,
-          // the only way the right 'this' object is passed to the formatter function
-
-          formatter: function lel() {
-            const endings = ['st', 'nd', 'rd'];
-            return `${this.point.x}${endings[this.point.x - 1] || 'th'} century<br />${
-              this.point.y
-            } occurences`;
-          },
-        },
-        legend: {
-          verticalAlign: 'top',
-        },
-        yAxis: {
-          title: {
-            text: '',
-          },
-        },
-        xAxis: {
-          labels: {
-            formatter: ({ value }) => value * 100,
-          },
-        },
-        series: this.data.map((x) => ({
-          name: x.title,
-          data: x.data,
-        })),
-      };
-    },
-  },
-};
-</script>
