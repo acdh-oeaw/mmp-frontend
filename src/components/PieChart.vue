b887dbc0aff9554fb6de9b35f9c3ad4528ea4c76
Stefan Probst <stefanprobst@posteo.de>
2023-01-29 22:54:21 +0100

    refactor: use composition api in pie chart

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,68 +1,69 @@
+<script lang="ts" setup>
+import { Chart } from 'highcharts-vue';
+import { computed, onMounted, ref } from 'vue';
+
+const props = defineProps<{
+  data: any;
+  title: any;
+  height: any;
+}>();
+
+const renderKey = ref(0);
+
+onMounted(() => {
+  renderKey.value -= -1; // this makes this component work, i dont know why
+});
+
+const pieOptions = computed(() => {
+  const reducedData = props.data.filter((x: any, i: number) => i < 25);
+
+  for (let i = 0; i < reducedData.length; i += 1) {
+    for (let j = 1; reducedData[i].length > 30; j += 1)
+      reducedData[i] = reducedData[i].filter((entry: any) => entry[1] > j); // improves performance by a lot
+  }
+
+  return {
+    chart: {
+      backgroundColor: 'transparent',
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie',
+      height: props.height || '500px',
+    },
+    title: {
+      text: props.title || false,
+    },
+    credits: {
+      enabled: false,
+    },
+    tooltip: {
+      pointFormat: 'Occurences: {point.y}',
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.y}',
+        },
+      },
+    },
+    series: {
+      name: 'words',
+      colorByPoint: true,
+      data: reducedData,
+    },
+  };
+});
+</script>
+
 <template>
   <div class="pieWrapper">
     <chart :options="pieOptions" />
   </div>
 </template>
-
-<script>
-import { Chart } from 'highcharts-vue';
-
-export default {
-  name: 'PieChart',
-  components: { Chart },
-  props: ['data', 'title', 'height'],
-  data: () => ({
-    renderKey: 0,
-  }),
-  computed: {
-    pieOptions() {
-      const reducedData = this.data.filter((x, i) => i < 25);
-      for (let i = 0; i < reducedData.length; i += 1) {
-        for (let j = 1; reducedData[i].length > 30; j += 1)
-          reducedData[i] = reducedData[i].filter((entry) => entry[1] > j); // improves performance by a lot
-      }
-
-      return {
-        chart: {
-          backgroundColor: 'transparent',
-          plotBackgroundColor: null,
-          plotBorderWidth: null,
-          plotShadow: false,
-          type: 'pie',
-          height: this.height || '500px',
-        },
-        title: {
-          text: this.title || false,
-        },
-        credits: {
-          enabled: false,
-        },
-        tooltip: {
-          pointFormat: 'Occurences: {point.y}',
-        },
-        plotOptions: {
-          pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-              enabled: true,
-              format: '<b>{point.name}</b>: {point.y}',
-            },
-          },
-        },
-        series: {
-          name: 'words',
-          colorByPoint: true,
-          data: reducedData,
-        },
-      };
-    },
-  },
-  mounted() {
-    this.renderKey -= -1; // this makes this component work, i dont know why
-  },
-};
-</script>
 
 <style>
 div.pieWrapper {
