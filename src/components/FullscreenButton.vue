5f5cd57ba765b85ca4796dd99f8d419dfc22c9d9
Stefan Probst <stefanprobst@posteo.de>
2023-01-29 20:10:39 +0100

    refactor: use composition api in full-screen-button

--- src/components/FullscreenButton.vue
+++ src/components/FullscreenButton.vue
@@ -1,3 +1,43 @@
+<script lang="ts" setup>
+import { useRoute, useRouter } from 'vue-router/composables';
+
+import type { CaseStudy } from '@/api';
+import { useFullScreen } from '@/lib/use-full-screen';
+
+const router = useRouter();
+const route = useRoute();
+
+const props = defineProps<{
+  usecase?: CaseStudy['id'];
+  left?: boolean;
+}>();
+
+function onClick() {
+  router.push({
+    name: isFullScreen.value
+      ? route.name?.replace(' Fullscreen', '')
+      : `${props.usecase ? getComponentFromTab(route.query.tab) : route.name} Fullscreen`,
+    // @ts-expect-error Check later.
+    query: props.usecase ? { ...route.query, 'Use Case': props.usecase } : route.query,
+    params: route.params,
+  });
+}
+
+function getComponentFromTab(tab: unknown) {
+  switch (tab) {
+    case 'cloud':
+      return 'Word Cloud';
+    case 'graph':
+      return 'Network Graph';
+    case 'map':
+    default:
+      return 'Map';
+  }
+}
+
+const isFullScreen = useFullScreen();
+</script>
+
 <template>
   <v-btn
     absolute
@@ -6,42 +46,9 @@
     :right="left === undefined"
     depressed
     icon
-    @click="press"
+    @click="onClick"
   >
     <v-icon v-if="isFullScreen">mdi-fullscreen-exit</v-icon>
     <v-icon v-else>mdi-fullscreen</v-icon>
   </v-btn>
 </template>
-
-<script>
-import helpers from '@/helpers';
-
-export default {
-  name: 'FullscreenButton',
-  mixins: [helpers],
-  props: ['usecase', 'left'],
-  methods: {
-    press() {
-      this.$router.push({
-        name: this.isFullScreen
-          ? this.$route.name.replace(' Fullscreen', '')
-          : `${
-              this.usecase ? this.getComponentFromTab(this.$route.query.tab) : this.$route.name
-            } Fullscreen`,
-        query: this.usecase
-          ? this.addParamsToQuery({ 'Use Case': this.usecase })
-          : this.$route.query,
-        params: this.$route.params,
-      });
-    },
-    getComponentFromTab(tab) {
-      const components = {
-        cloud: 'Word Cloud',
-        graph: 'Network Graph',
-        map: 'Map',
-      };
-      return components[tab];
-    },
-  },
-};
-</script>
