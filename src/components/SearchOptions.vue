e51a42ce7d1ad4a65b51075b28167079cb3ce86c
Stefan Probst <stefanprobst@posteo.de>
2023-02-01 16:19:39 +0100

    refactor: use composition api in search options

--- src/components/SearchOptions.vue
+++ src/components/SearchOptions.vue
@@ -1,3 +1,29 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+import { useStore } from '@/lib/use-store';
+
+const store = useStore();
+
+const hasUsecase = computed({
+  get() {
+    return store.state.apiParams.hasUsecase;
+  },
+  set(val) {
+    store.commit('setApiParam', { key: 'hasUsecase', val });
+  },
+});
+
+const intersection = computed({
+  get() {
+    return store.state.apiParams.intersect;
+  },
+  set(val) {
+    store.commit('setApiParam', { key: 'intersect', val });
+  },
+});
+</script>
+
 <template>
   <v-card class="option-card">
     <v-card-title>Search Options</v-card-title>
@@ -36,161 +62,9 @@
           </template>
         </v-radio>
       </v-radio-group>
-      <v-card-title>Filter entity types in search bar</v-card-title>
-      <v-checkbox
-        v-model="all.model"
-        label="All"
-        :indeterminate="all.indeterminate"
-        @change="$store.commit('setAllFilters', $event)"
-      />
-      <v-divider />
-      <v-checkbox
-        v-model="filters.author"
-        label="Author"
-        color="red lighten-1"
-        @change="changeValue($event, 'author')"
-      />
-      <v-divider />
-      <v-checkbox
-        v-model="filters.passage"
-        label="Passage"
-        color="teal lighten-2"
-        @change="changeValue($event, 'passage')"
-      />
-      <v-divider />
-      <v-checkbox
-        v-model="keyword.model"
-        label="Keyword"
-        :indeterminate="keyword.indeterminate"
-        color="blue lighten-2"
-        @change="changeSubCats($event, 'keyword')"
-      />
-      <v-checkbox
-        v-model="filters.keyword.name"
-        label="Name"
-        color="blue lighten-2"
-        prepend-icon="mdi-chevron-right"
-        dense
-        @change="changeValue($event, 'keyword.name')"
-      />
-      <v-checkbox
-        v-model="filters.keyword.phrase"
-        label="Keyword"
-        color="blue lighten-2"
-        prepend-icon="mdi-chevron-right"
-        dense
-        @change="changeValue($event, 'keyword.phrase')"
-      />
-      <v-checkbox
-        v-model="filters.keyword.ethnonym"
-        label="Ethnonym"
-        color="blue lighten-2"
-        prepend-icon="mdi-chevron-right"
-        dense
-        @change="changeValue($event, 'keyword.ethnonym')"
-      />
-      <v-checkbox
-        v-model="filters.keyword.region"
-        label="Region"
-        color="blue lighten-2"
-        prepend-icon="mdi-chevron-right"
-        dense
-        @change="changeValue($event, 'keyword.phrase')"
-      />
-      <v-divider />
-      <v-checkbox
-        v-model="filters.usecase"
-        label="Use Case"
-        color="amber lighten-1"
-        @change="changeValue($event, 'usecase')"
-      />
-      <v-divider />
-      <v-checkbox
-        v-model="filters.place"
-        label="Place"
-        color="green lighten-1"
-        @change="changeValue($event, 'place')"
-      />
     </v-card-text>
   </v-card>
 </template>
-
-<script>
-export default {
-  name: 'SearchOptionDialog',
-  data: () => ({
-    all: {
-      model: true,
-      indeterminate: false,
-    },
-    keyword: {
-      model: true,
-      indeterminate: false,
-    },
-  }),
-  computed: {
-    filters() {
-      return this.$store.state.searchFilters;
-    },
-    hasUsecase: {
-      get() {
-        return this.$store.state.apiParams.hasUsecase;
-      },
-      set(val) {
-        this.$store.commit('setApiParam', { key: 'hasUsecase', val });
-      },
-    },
-    intersection: {
-      get() {
-        return this.$store.state.apiParams.intersect;
-      },
-      set(val) {
-        this.$store.commit('setApiParam', { key: 'intersect', val });
-      },
-    },
-    active: {
-      get() {
-        return this.$store.state.interface.searchOptions;
-      },
-      set() {
-        this.$store.commit('toggleOptions');
-      },
-    },
-  },
-  watch: {
-    filters: {
-      handler(val) {
-        this.handleCategories('keyword', Object.values(this.filters.keyword));
-
-        let allArray = [];
-        Object.entries(val).forEach((entry) => {
-          if (typeof entry[1] === 'object') allArray = allArray.concat(Object.values(entry[1]));
-          else allArray.push(entry[1]);
-        });
-        this.handleCategories('all', allArray);
-      },
-      deep: true,
-      // immediate: true,
-    },
-  },
-  methods: {
-    changeSubCats(val, cat) {
-      this.$store.commit('setSubFilters', { cat, val });
-    },
-    changeValue(val, cat) {
-      // console.log(val, cat);
-      this.$store.commit('setFilter', { cat, val });
-    },
-    handleCategories(cat, vals) {
-      // console.log('filter vals', vals);
-      this[cat].indeterminate = false;
-      if (vals.every((x) => x)) this[cat].model = true;
-      else if (vals.every((x) => !x)) this[cat].model = false;
-      else this[cat].indeterminate = true;
-    },
-  },
-};
-</script>
 
 <style scoped>
 .option-card {
