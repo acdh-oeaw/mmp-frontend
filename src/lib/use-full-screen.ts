80add2378d896afa646968a3679609c71ef08fbb
Stefan Probst <stefanprobst@posteo.de>
2023-02-09 09:22:12 +0100

    fix: add fullscreen hook

---
+++ src/lib/use-full-screen.ts
@@ -0,0 +1,13 @@
+import { computed } from 'vue';
+
+import { useViewMode } from '@/lib/use-view-mode';
+
+export function useFullScreen() {
+	const { searchFilters } = useViewMode();
+
+	const isFullScreen = computed(() => {
+		return searchFilters.value['view-mode'] === 'fullscreen';
+	});
+
+	return isFullScreen;
+}
