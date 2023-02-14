f8feabf1f2111db25dc51eca4bcac7c1b0adf60b
Stefan Probst <stefanprobst@posteo.de>
2023-02-14 08:56:34 +0100

    feat: rehype

--- src/server/api/story/[id].ts
+++ src/server/api/story/[id].ts
@@ -1,9 +1,14 @@
+import { assert } from "@stefanprobst/assert";
 import { HttpError } from "@stefanprobst/request";
+import fromHtml from "rehype-parse";
+import { unified } from "unified";
 import { z } from "zod";
 
 import { getCaseStudyById } from "@/api";
 
 const schema = z.object({ id: z.coerce.number().int() });
+
+const processor = unified().use(fromHtml);
 
 export default defineEventHandler(async (event) => {
 	const params = schema.safeParse(event.context.params);
@@ -14,8 +19,12 @@ export default defineEventHandler(async (event) => {
 
 	try {
 		const story = await getCaseStudyById({ id: params.data.id });
+		const html = story.story_map;
+		assert(html != null);
 
-		return { story };
+		const tree = processor.parse(html);
+
+		return { tree };
 	} catch (error) {
 		const statusCode = error instanceof HttpError ? error.response.status : 500;
 		throw createError({ statusCode });
