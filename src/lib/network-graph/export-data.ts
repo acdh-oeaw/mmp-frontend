7c5716e7091b811984dfa5e5529f39f4dce184c6
Stefan Probst <stefanprobst@posteo.de>
2023-02-21 10:11:34 +0100

    chore: wip

--- src/lib/network-graph/export-data.ts
+++ src/lib/network-graph/export-data.ts
@@ -12,16 +12,73 @@ function saveAsImage(element: HTMLElement) {
 }
 
 function saveAsJson(graph: NetworkGraphData) {
-	const blob = createBlob(
-		JSON.stringify(
-			{
-				nodes: Array.from(graph.nodes),
-				edges: Array.from(graph.edges),
-			},
-			null,
-			2,
-		),
-		"application/json",
+	const json = JSON.stringify(
+		{ nodes: Array.from(graph.nodes), edges: Array.from(graph.edges) },
+		null,
+		2,
 	);
+	const blob = createBlob(json, "application/json");
 	downloadFile(blob, "graph.json");
+}
+
+function saveAsCsv(graph: NetworkGraphData) {
+	const headings = ["ID", "Label", "Kind", "Type", "Neighbors"].join(",");
+
+	function sanitize(value: string) {
+		return value.replace(",", " ");
+	}
+
+	interface CsvRow {
+		id: number;
+		label: string;
+		kind: string;
+		type: string;
+		neighbors: string;
+	}
+
+	const rows: Array<CsvRow> = [];
+
+	graph.nodes.forEach((node) => {
+		const neighbors: Array<string> = [];
+
+		node.neighbors.forEach((key) => {
+			const neighbor = graph.nodes.get(key);
+			if (neighbor != null) {
+				neighbors.push(sanitize(neighbor.label));
+			}
+		});
+
+		const row: CsvRow = {
+			id: node.id,
+			label: sanitize(node.label),
+			kind: node.kind,
+			type: "type" in node ? node.type : "",
+			neighbors: neighbors.join("/"),
+		};
+
+		rows.push(row);
+	});
+
+	const csv = [headings, rows].join("\n");
+	const blob = createBlob(csv, "text/csv");
+	downloadFile(blob, "graph.csv");
+}
+
+function saveAsGexf(graph: NetworkGraphData) {
+	return Promise.all([import("graphology"), import("graphology-gexf/browser")]).then(
+		([{ DirectedGraph }, { write }]) => {
+			const _graph = new DirectedGraph();
+
+			graph.nodes.forEach((node) => {
+				_graph.addNode(node.key, node);
+			});
+			graph.edges.forEach((edge) => {
+				_graph.addEdgeWithKey(edge.key, edge.source, edge.target, edge);
+			});
+
+			const xml = write(_graph);
+			const blob = createBlob(xml, "application/xml");
+			downloadFile(blob, "graph.gexf");
+		},
+	);
 }
