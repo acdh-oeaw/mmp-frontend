f48bb52391312b19cb0a85a691bee6d142ebcf97
Stefan Probst <stefanprobst@posteo.de>
2023-02-25 13:14:08 +0100

    chore: wip

--- src/pages/case-studies/[id]/texts-by-authors.vue
+++ src/pages/case-studies/[id]/texts-by-authors.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { ChevronDownIcon } from "@heroicons/vue/20/solid";
+
 import ErrorMessage from "@/components/error-message.vue";
 import KeywordTag from "@/components/keyword-tag.vue";
 import LoadingIndicator from "@/components/loading-indicator.vue";
@@ -44,60 +47,79 @@ const { data, isLoading, isError, isEmpty, isFetching } = useTextsByAuthors(sear
 				<LoadingIndicator>Updating passages...</LoadingIndicator>
 			</template>
 
-			<!-- TODO: some of those nested unordered lists could be definition lists. -->
-			<ul rol="list">
-				<li v-for="[authorId, author] of data.authorsById" :key="authorId">
-					<h3>{{ getAuthorLabel(author) }}</h3>
+			<div class="overflow-x-auto transition" :class="{ 'grayscale opacity-50': isFetching }">
+				<!-- TODO: some of those nested unordered lists could be definition lists. -->
+				<ul rol="list" class="min-w-full divide-y divide-neutral-200 text-sm">
+					<li
+						v-for="[authorId, author] of data.authorsById"
+						:key="authorId"
+						class="grid gap-2 px-6 py-4 text-neutral-800"
+					>
+						<h3 class="text-lg font-medium">{{ getAuthorLabel(author) }}</h3>
 
-					<span>
-						{{ data.textIdsByAuthorId.get(authorId)?.size }} texts and
-						{{ data.passageIdsByAuthorId.get(author.id)?.size }} passages
-					</span>
+						<div class="text-xs font-medium uppercase text-neutral-500">
+							{{ data.textIdsByAuthorId.get(authorId)?.size }} texts and
+							{{ data.passageIdsByAuthorId.get(author.id)?.size }} passages
+						</div>
 
-					<ul role="list">
-						<li v-for="textId of data.textIdsByAuthorId.get(authorId)" :key="textId">
-							<span>{{ data.textsById.get(textId)?.title }}</span>
-
-							<span>{{ data.passageIdsByTextTd.get(textId)?.size }} passages</span>
-
-							<ul role="list">
-								<li v-for="passageId of data.passageIdsByTextTd.get(textId)" :key="passageId">
-									<NuxtLink
-										:href="{
-											path: '/explore/search-results',
-											query: createSearchFilterParams({
-												...defaultSearchFilters,
-												passage: [passageId],
-											}),
-										}"
-									>
-										{{ data.passagesById.get(passageId)?.display_label }}
-									</NuxtLink>
-
-									<ul role="list">
-										<li
-											v-for="keyword of data.passagesById.get(passageId)?.key_word"
-											:key="keyword.id"
-										>
-											<NuxtLink
-												:href="{
-													path: '/explore/search-results',
-													query: createSearchFilterParams({
-														...defaultSearchFilters,
-														keyword: [keyword.id],
-													}),
-												}"
+						<ul role="list" class="my-1 grid gap-2">
+							<li v-for="textId of data.textIdsByAuthorId.get(authorId)" :key="textId">
+								<Disclosure as="div" class="grid gap-2">
+									<DisclosureButton class="flex w-full items-center justify-between">
+										<span>
+											{{ data.textsById.get(textId)?.title }} ({{
+												data.passageIdsByTextTd.get(textId)?.size
+											}}
+											passages)
+										</span>
+										<ChevronDownIcon class="h-5 w-5" />
+									</DisclosureButton>
+									<DisclosurePanel>
+										<ul role="list" class="grid gap-4 px-4 pb-2">
+											<li
+												v-for="passageId of data.passageIdsByTextTd.get(textId)"
+												:key="passageId"
+												class="grid gap-1"
 											>
-												<KeywordTag :keyword="keyword" />
-											</NuxtLink>
-										</li>
-									</ul>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-			</ul>
+												<NuxtLink
+													:href="{
+														path: '/explore/search-results',
+														query: createSearchFilterParams({
+															...defaultSearchFilters,
+															passage: [passageId],
+														}),
+													}"
+												>
+													{{ data.passagesById.get(passageId)?.display_label }}
+												</NuxtLink>
+
+												<ul class="flex flex-wrap gap-0.5" role="list">
+													<li
+														v-for="keyword of data.passagesById.get(passageId)?.key_word"
+														:key="keyword.id"
+													>
+														<NuxtLink
+															:href="{
+																path: '/explore/search-results',
+																query: createSearchFilterParams({
+																	...defaultSearchFilters,
+																	keyword: [keyword.id],
+																}),
+															}"
+														>
+															<KeywordTag :keyword="keyword" />
+														</NuxtLink>
+													</li>
+												</ul>
+											</li>
+										</ul>
+									</DisclosurePanel>
+								</Disclosure>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
 		</template>
 	</div>
 </template>
