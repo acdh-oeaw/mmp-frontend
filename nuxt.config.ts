402bf9cf700aabe94a8f8dc5c7acc1ecd8500c24
Stefan Probst <stefanprobst@posteo.de>
2023-02-27 19:59:36 +0100

    fix: set lang attribute on html element

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,9 @@ export default defineNuxtConfig({
 	},
 	app: {
 		head: {
+			htmlAttrs: {
+				lang: "en",
+			},
 			link: [
 				{ href: "/favicon.ico", rel: "icon", sizes: "any" },
 				{ href: "/apple-touch-icon.png", rel: "apple-touch-icon" },
