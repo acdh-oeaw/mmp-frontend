3c4cc7d0f854a1ac60c6396f763a19cf11418ac7
stefanprobst <stefanprobst@posteo.de>
2023-03-14 17:52:30 +0100

    feat: add separate by-id pages for entities (#176)

---
+++ src/pages/entities/texts/[id].vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+
+import TextDetails from "@/components/text-details.vue";
+import { useResourceIdParam } from "@/lib/use-resource-id-param";
+import { useHead } from "#imports";
+
+const title = "Text";
+
+useHead({
+	title,
+	meta: [{ property: "og:title", content: title }],
+});
+
+const id = useResourceIdParam();
+const ids = computed(() => {
+	return new Set([id.value]);
+});
+</script>
+
+<template>
+	<div class="relative mx-auto h-full w-full">
+		<h2 class="sr-only">Text</h2>
+
+		<div>
+			<TextDetails :ids="ids" />
+		</div>
+	</div>
+</template>
