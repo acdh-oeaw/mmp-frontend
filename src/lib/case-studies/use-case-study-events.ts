0b27f88f8dd83893049ed961ad5674a8c0952f48
Robin Kaggl <robin@kaggl.co>
2023-03-14 17:29:33 +0100

    added case study specific events to timeline

---
+++ src/lib/case-studies/use-case-study-events.ts
@@ -0,0 +1,26 @@
+import { type ComputedRef, computed } from "vue";
+
+import { type CaseStudy, useEvents } from "@/api";
+
+export function useCaseStudyEvent(id: ComputedRef<CaseStudy["id"]>) {
+	const caseStudyEventQuery = useEvents({ use_case: [id], limit: 100, ordering: "start_date" });
+	const isFetching = caseStudyEventQuery.isFetching;
+	const isLoading = caseStudyEventQuery.isInitialLoading;
+	const isError = caseStudyEventQuery.isError;
+	const caseStudyEvents = computed(() => {
+		return caseStudyEventQuery.data.value ?? [];
+	});
+
+	const isEmpty = computed(() => {
+		return Object(caseStudyEvents).count === 0;
+	});
+
+	return {
+		caseStudyEvents,
+		caseStudyEventQuery,
+		isEmpty,
+		isError,
+		isFetching,
+		isLoading,
+	};
+}
--- src/pages/case-studies/[id]/timeline.vue
+++ src/pages/case-studies/[id]/timeline.vue
@@ -1,18 +1,20 @@
 <script lang="ts" setup>
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/vue";
 import {
 	BookOpenIcon as TextIcon,
 	CalendarDaysIcon as EventIcon,
 	ChevronRightIcon,
 	PencilIcon as AuthorIcon,
 } from "@heroicons/vue/24/outline";
+import { computed } from "vue";
 
 import { type GetCaseStudyTimetableById } from "@/api";
 import Centered from "@/components/centered.vue";
 import ErrorMessage from "@/components/error-message.vue";
 import LoadingIndicator from "@/components/loading-indicator.vue";
-import NothingFoundMessage from "@/components/nothing-found-message.vue";
+import { useCaseStudyEvent } from "@/lib/case-studies/use-case-study-events";
 import { useCaseStudyIdParam } from "@/lib/case-studies/use-case-study-id-param";
-import { useCaseStudyTimeline } from "@/lib/case-studies/use-case-study-timeline";
+import { useCaseStudyTimeline } from "@/lib/case-studies/use-case-study-timeline.js";
 import { getDateRangeLabel } from "@/lib/get-label";
 import { useSearchFilters } from "@/lib/search/use-search-filters";
 import { NuxtLink } from "#components";
@@ -26,7 +28,27 @@ useHead({
 });
 
 const id = useCaseStudyIdParam();
-const { caseStudyTimeline, isEmpty, isError, isFetching, isLoading } = useCaseStudyTimeline(id);
+const timelineLib = useCaseStudyTimeline(id);
+const eventLib = useCaseStudyEvent(id);
+
+const timeline = computed(() => {
+	return timelineLib.caseStudyTimeline.value;
+});
+
+const events = computed(() => {
+	return eventLib.caseStudyEvents.value;
+});
+
+const isLoading = computed(() => {
+	return eventLib.isLoading.value || timelineLib.isLoading.value;
+});
+const isError = computed(() => {
+	return eventLib.isError.value || timelineLib.isError.value;
+});
+const isFetching = computed(() => {
+	return eventLib.isFetching.value || timelineLib.isFetching.value;
+});
+
 const { createSearchFilterParams, defaultSearchFilters } = useSearchFilters();
 
 function getEventColor(type: GetCaseStudyTimetableById.Response[number]["ent_type"]) {
@@ -56,65 +78,102 @@ function getEventColor(type: GetCaseStudyTimetableById.Response[number]["ent_typ
 				<ErrorMessage>Failed to load timeline.</ErrorMessage>
 			</Centered>
 		</template>
-
-		<template v-else-if="isEmpty">
-			<Centered>
-				<NothingFoundMessage></NothingFoundMessage>
-			</Centered>
-		</template>
-
 		<template v-else>
 			<template v-if="isFetching">
 				<Centered>
 					<LoadingIndicator>Updating timeline...</LoadingIndicator>
 				</Centered>
 			</template>
-
-			<ol
-				role="list"
-				class="grid items-center gap-6 transition-all"
-				style="grid-template-columns: repeat(3, auto)"
-				:class="{ 'opacity-50 grayscale': isFetching }"
-			>
-				<template v-for="(event, i) of caseStudyTimeline" :key="event.id">
-					<div class="text-right">
-						<span>{{ getDateRangeLabel(event.start_date, event.end_date) }}</span>
-					</div>
-					<div class="relative h-full min-h-[3rem] w-12">
-						<span
-							class="absolute top-2/4 left-2/4 h-12 w-12 -translate-x-2/4 -translate-y-2/4 rounded-full border-4 border-white p-2 text-white shadow"
-							:class="getEventColor(event.ent_type)"
-						>
-							<EventIcon v-if="event.ent_type === 'event'" />
-							<AuthorIcon v-if="event.ent_type === 'autor'" />
-							<TextIcon v-if="event.ent_type === 'text'" />
-						</span>
-						<div
-							v-if="i + 1 < caseStudyTimeline.length"
-							class="absolute inset-x-2/4 top-0 bottom-[-1.5rem] -z-10 border border-neutral-300"
-						/>
-					</div>
-					<NuxtLink
-						v-if="event.ent_type === 'autor'"
-						:href="{
-							path: '/explore/search-results',
-							query: createSearchFilterParams({ ...defaultSearchFilters, author: [event.id] }),
-						}"
+			<TabGroup>
+				<TabList v-slot="{ selectedIndex }" class="mb-4 flex justify-around border-b">
+					<Tab
+						v-for="(label, i) in ['All events', 'Case study specific events']"
+						:key="label"
+						class="rounded-t p-2 transition hover:bg-neutral-100"
+						:class="{ 'bg-neutral-200': i === selectedIndex }"
 					>
-						<div
-							class="flex w-fit basis-0 items-center rounded p-2 transition hover:bg-neutral-200"
+						{{ label }}
+					</Tab>
+				</TabList>
+				<TabPanels>
+					<TabPanel>
+						<ol
+							role="list"
+							class="grid items-center gap-6 transition-all"
+							style="grid-template-columns: repeat(3, auto)"
+							:class="{ 'opacity-50 grayscale': isFetching }"
 						>
-							<span>
-								{{ event.ent_title }}
-							</span>
-							<span>
-								<ChevronRightIcon class="ml-2 inline h-5 w-5" />
-							</span>
-						</div>
-					</NuxtLink>
-					<span v-else>{{ event.ent_description }}</span>
-				</template>
-			</ol>
+							<template v-for="(event, i) of timeline" :key="event.id">
+								<div class="text-right">
+									<span>{{ getDateRangeLabel(event.start_date, event.end_date) }}</span>
+								</div>
+								<div class="relative h-full min-h-[3rem] w-12">
+									<span
+										class="absolute top-2/4 left-2/4 h-12 w-12 -translate-x-2/4 -translate-y-2/4 rounded-full border-4 border-white p-2 text-white shadow"
+										:class="getEventColor(event.ent_type)"
+									>
+										<EventIcon v-if="event.ent_type === 'event'" />
+										<AuthorIcon v-if="event.ent_type === 'autor'" />
+										<TextIcon v-if="event.ent_type === 'text'" />
+									</span>
+									<div
+										v-if="i + 1 < timeline.length"
+										class="absolute inset-x-2/4 top-0 bottom-[-1.5rem] -z-10 border border-neutral-300"
+									/>
+								</div>
+								<NuxtLink
+									v-if="event.ent_type === 'autor'"
+									:href="{
+										path: '/explore/search-results',
+										query: createSearchFilterParams({
+											...defaultSearchFilters,
+											author: [event.id],
+										}),
+									}"
+								>
+									<div
+										class="flex w-fit basis-0 items-center rounded p-2 transition hover:bg-neutral-200"
+									>
+										<span>
+											{{ event.ent_title }}
+										</span>
+										<span>
+											<ChevronRightIcon class="ml-2 inline h-5 w-5" />
+										</span>
+									</div>
+								</NuxtLink>
+								<span v-else>{{ event.ent_description }}</span>
+							</template>
+						</ol>
+					</TabPanel>
+					<TabPanel>
+						<ol
+							role="list"
+							class="grid items-center gap-6 transition-all"
+							style="grid-template-columns: repeat(3, auto)"
+							:class="{ 'opacity-50 grayscale': isFetching }"
+						>
+							<template v-for="(event, i) of Object(events).results" :key="event.id">
+								<div class="text-right">
+									<span>{{ getDateRangeLabel(event.start_date, event.end_date) }}</span>
+								</div>
+								<div class="relative h-full min-h-[3rem] w-12">
+									<span
+										class="absolute top-2/4 left-2/4 h-12 w-12 -translate-x-2/4 -translate-y-2/4 rounded-full border-4 border-white bg-green-500 p-2 text-white shadow"
+									>
+										<EventIcon />
+									</span>
+									<div
+										v-if="i + 1 < Object(events).results.length"
+										class="absolute inset-x-2/4 top-0 bottom-[-1.5rem] -z-10 border border-neutral-300"
+									/>
+								</div>
+								<span>{{ event.description || "No Description" }}</span>
+							</template>
+						</ol>
+					</TabPanel>
+				</TabPanels>
+			</TabGroup>
 		</template>
 	</div>
 </template>
