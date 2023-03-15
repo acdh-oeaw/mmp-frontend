c10eb88f70c3a343978f461efb6c6872eaf21d99
Stefan Probst <stefanprobst@posteo.de>
2023-03-15 08:53:07 +0100

    fix: fix node selection

--- src/lib/network-graph/use-network-graph-events.ts
+++ src/lib/network-graph/use-network-graph-events.ts
@@ -1,5 +1,6 @@
 import { computed, ref } from "vue";
 
+import { type ResourceKey } from "@/api";
 import { type NetworkGraphNode } from "@/lib/network-graph/network-graph.types";
 import { type SelectionKey, createSelectionKey } from "@/lib/search/selection-key";
 import { useSearchFilters } from "@/lib/search/use-search-filters";
@@ -11,12 +12,12 @@ export function useNetworkGraphEvents() {
 	const { createSearchFilterParams, searchFilters } = useSearchFilters();
 	const { createSelectionParams, selection } = useSelection();
 	const selectedKeys = computed<Set<SelectionKey>>(() => {
-		const keys = new Set();
+		const keys = new Set<ResourceKey>();
 		const prefix = "graph-".length;
 
 		selection.value.selection.forEach((key) => {
 			if (key.startsWith("graph-author") || key.startsWith("graph-keyword")) {
-				keys.add(key.slice(prefix));
+				keys.add(key.slice(prefix) as ResourceKey);
 			}
 		});
 
@@ -30,7 +31,7 @@ export function useNetworkGraphEvents() {
 			kind: node.kind === "autor" ? "graph-author" : "graph-keyword",
 			id: node.id,
 		});
-		const _selection = new Set(selectedKeys.value);
+		const _selection = new Set(selection.value.selection);
 		if (_selection.has(key)) {
 			_selection.delete(key);
 		} else {
