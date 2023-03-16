4d88866542021aa83167ab059bc9c1e341fc6929
Stefan Probst <stefanprobst@posteo.de>
2023-03-16 13:22:42 +0100

    feat: add 404 and 500 pages

---
+++ src/pages/[...404].vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import Centered from "@/components/centered.vue";
+import MainContent from "@/components/main-content.vue";
+</script>
+
+<template>
+	<MainContent>
+		<Centered>
+			<div class="grid gap-2">
+				<span>This page does not exist.</span>
+			</div>
+		</Centered>
+	</MainContent>
+</template>
