618df326198d2501743db56806223acafca65963
Stefan Probst <stefanprobst@posteo.de>
2023-08-31 19:13:06 +0200

    refactor: use new imprint service

--- config/imprint.config.ts
+++ config/imprint.config.ts
@@ -3,16 +3,11 @@ import { createUrl } from "@stefanprobst/request";
 import { env } from "~/config/env.config";
 import { metadata } from "~/config/metadata.config";
 
-const baseUrl = "https://shared.acdh.oeaw.ac.at";
-const pathname = "/acdh-common-assets/api/imprint.php";
 const redmineId = env.VITE_APP_REDMINE_ID;
 const locale = metadata.locale;
 
 export const url = createUrl({
-	baseUrl,
-	pathname,
-	searchParams: {
-		outputLang: locale,
-		serviceID: redmineId,
-	},
+	baseUrl: "https://imprint.acdh.oeaw.ac.at",
+	pathname: `/${redmineId}`,
+	searchParams: { locale },
 });
