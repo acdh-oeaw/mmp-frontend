5f8fd0f9a42f02fb90ab104ad021d857a21b2653
Stefan Probst <stefanprobst@posteo.de>
2024-04-30 15:56:54 +0200

    feat: add sitemap

---
+++ server/routes/sitemap.xml.get.ts
@@ -0,0 +1,78 @@
+import { join } from "node:path";
+
+import { defaultContentType, defineEventHandler } from "#imports";
+import { getCaseStudies } from "@/api";
+import { createUrl } from "@acdh-oeaw/lib";
+import fg from "fast-glob";
+import { z } from "zod";
+
+const baseUrl = z.string().url().parse(process.env.NUXT_PUBLIC_APP_BASE_URL);
+
+async function createSitemap() {
+	// eslint-disable-next-line import/no-named-as-default-member
+	const paths = fg.globSync("./**/*.vue", { cwd: join(process.cwd(), "pages") });
+
+	const routes: Array<string> = [];
+
+	paths.forEach((path) => {
+		const _route = path.slice(0, -".vue".length).replace(/(\/)?index$/, "");
+		const route = _route.length === 0 ? _route : "/" + _route;
+
+		const segments = [];
+
+		for (const segment of route.split("/")) {
+			/** Dynamic routes. */
+			if (segment.startsWith("[") && segment.endsWith("]")) return;
+
+			segments.push(segment);
+		}
+
+		routes.push(segments.join("/"));
+	});
+
+	const entries: Array<{ url: string; lastModified?: Date }> = routes.map((pathname) => {
+		console.log(baseUrl);
+		return {
+			url: String(createUrl({ baseUrl, pathname })),
+			// lastModified: new Date(),
+		};
+	});
+
+	const caseStudies = await getCaseStudies({ limit: 1000 });
+	for (const caseStudy of caseStudies.results) {
+		const url = createUrl({ baseUrl, pathname: `/case-studies/${caseStudy.id}/` });
+
+		entries.push({ url: String(createUrl({ baseUrl: url, pathname: "timeline" })) });
+		entries.push({ url: String(createUrl({ baseUrl: url, pathname: "story" })) });
+		entries.push({
+			url: String(createUrl({ baseUrl: url, pathname: "network-graph-visualisation" })),
+		});
+		entries.push({ url: String(createUrl({ baseUrl: url, pathname: "geo-map-visualisation" })) });
+		entries.push({
+			url: String(createUrl({ baseUrl: url, pathname: "word-cloud-visualisation" })),
+		});
+		entries.push({ url: String(createUrl({ baseUrl: url, pathname: "texts-by-authors" })) });
+	}
+
+	const sitemap = [
+		'<?xml version="1.0" encoding="UTF-8"?>',
+		'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+		...entries.flatMap((entry) => {
+			const elements = ["<url>", `<loc>${entry.url}</loc>`];
+			if (entry.lastModified != null) {
+				elements.push(`<lastmod>${entry.lastModified.toISOString()}</lastmod>`);
+			}
+			elements.push("</url>");
+			return elements;
+		}),
+		"</urlset>",
+	].join("\n");
+
+	return sitemap;
+}
+
+export default defineEventHandler(async (event) => {
+	const sitemap = await createSitemap();
+	defaultContentType(event, "application/xml");
+	return sitemap;
+});
